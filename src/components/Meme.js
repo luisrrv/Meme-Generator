b014cad11afdf8b77882fc6138b03fc29d3ee818
luis rodriguez <rodluisr@gmail.com>
2022-06-26 14:41:19 +0900

    added instructions

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -64,8 +64,8 @@ const Meme = (props) => {
     </div>
     <div className="meme">
       <img className="generated--meme" src={meme.randomImage} alt='meme' />
-      <h2 className="meme--text top">{meme.topText}</h2>
-      <h2 className="meme--text bottom">{meme.bottomText}</h2>
+      <h2 className="meme--text top">{meme.randomImage === logo ? "write your top/bottom text" :meme.topText}</h2>
+      <h2 className="meme--text bottom">{meme.randomImage == logo ? "then click generate to get meme image" :meme.bottomText}</h2>
     </div>
   </main>
   )
